a90a4e90ddd1e9372302a6d22ae600cd8fd6842d
Jung Hoan Kim <kjunghoan@gmail.com>
2023-07-28 20:16:05 -0400

    Co-authored-by: Kevin Glidden <gliddensolutions@gmail.com> Co-authored-by: Kayla Loizzo <kloizzo@users.noreply.github.com> Co-authored-by: helperbee <helperbee@users.noreply.github.com> Co-authored-by: liam-mceneaney <liam-mceneaney@users.noreply.github.com>

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { faker } from "faker-js/faker";
 const prisma = new PrismaClient();
 
-const seedData = ()=> {
+const seedDataConstructor = () => {
   return {
     firstname: faker.dataType.string(),
     lastName: faker.dataType.string(),
@@ -13,17 +13,16 @@ const seedData = ()=> {
         title: faker.dataType.string(),
         body: faker.dataType.string(),
         createdAt: faker.date.past(),
-        
-      }
-
-    ]
-  }
-}
-
+      },
+    ],
+  };
+};
+//written in part by mike g
+const seedData = faker.helpers.multiple(seedDataConstructor, { count: 5 });
 async function main() {
   // ... you will write your Prisma Client queries here
   await prisma.users.createMany({
-    data: 
+    data: seedData,
   });
 }
 

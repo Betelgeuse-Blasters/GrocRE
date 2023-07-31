7789c3da73e6dacea5b36fd9dca4f4744d638e26
Kiel Fuller <kpfuller28@gmail.com>
2023-07-31 16:04:14 -0400

    feat: comments feature janky

--- frontEnd/src/pages/Social/components/Post.jsx
+++ frontEnd/src/pages/Social/components/Post.jsx
@@ -1,12 +1,15 @@
-import { Card, Avatar, Collapse, message, Button } from 'antd';
+import { Card, Avatar, Collapse, message, Button, List } from 'antd';
 import { LikeOutlined, DislikeOutlined, CommentOutlined, HeartOutlined } from "@ant-design/icons";
+import {useState} from 'react';
 const {Meta} = Card;
 
 export default function Post() {
   const mealTitle = 'Cheesieburger';
   const username = 'username';
   const postTitle = `${mealTitle} by ${username}`;
+  const [commentsOpen, setCommentsOpen] = useState(false)
   const messageTime = 2.5;
+  const Cheesieburger = 'https://www.allrecipes.com/thmb/5JVfA7MxfTUPfRerQMdF-nGKsLY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/25473-the-perfect-basic-burger-DDMFS-4x3-56eaba3833fd4a26a82755bcd0be0c54.jpg'
 
   function savePost() {
     message.success(`${mealTitle} added to Saved Meals`, messageTime)
@@ -19,50 +22,124 @@ export default function Post() {
     }
   }
 
+  function openComment() {
+    setCommentsOpen(!commentsOpen);
+  }
+  const comments = [{
+    username: 'Test User One',
+    comment: 'test comment one'
+  },
+  {
+    username: 'Test User Two',
+    comment: 'test comment two'
+  }]
+
   return (
-    <Card
-      style={{width:600}}
-      cover={
-        <img alt = 'example' src="https://www.allrecipes.com/thmb/5JVfA7MxfTUPfRerQMdF-nGKsLY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/25473-the-perfect-basic-burger-DDMFS-4x3-56eaba3833fd4a26a82755bcd0be0c54.jpg"/>
-      }
-      actions={[
-        <LikeOutlined onClick={()=>{like(1)}} key='like'/>,
-        <DislikeOutlined  onClick={()=>{like(0)}} key='dislike'/>,
-        <CommentOutlined key='comments'/>
-      ]}
-      hoverable
-    >
-      <Meta
-        avatar={<Avatar src="https://www.allrecipes.com/thmb/5JVfA7MxfTUPfRerQMdF-nGKsLY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/25473-the-perfect-basic-burger-DDMFS-4x3-56eaba3833fd4a26a82755bcd0be0c54.jpg" />}
-        title={postTitle}
-        description='A Royale with Cheese'
-      />
-      <Button onClick={savePost} type='primary' icon={<HeartOutlined />} style={{position: 'absolute', top:'1%', right: '2%'}} />
-      <Collapse items={[
-        {
-          key: '1',
-          label: 'Nutritional Info',
-          children: <p>Some info that is very nutritious</p>
-        },
-        {
-          key: '2',
-          label: 'Ingredients',
-          children: <p>Definitely not random pieces of food from the trash</p>
-        },
-        {
-          key: '3',
-          label: 'Recipe/Steps',
-          children: <p>Get food, add fire, ?, profit</p>
-        },
-        {
-          key: '4',
-          label: 'Comments',
-          children: <p>This heckin sick bro</p>
+    <div>
+      <Card
+        style={{width:600}}
+        cover={
+          <img alt = 'example' src={Cheesieburger}/>
         }
-      ]}
-      bordered={false}
-      style={{backgroundColor:"white", paddingTop:'10px'}}
-      />
-    </Card>
+        actions={[
+          <LikeOutlined onClick={()=>{like(1)}} key='like'/>,
+          <DislikeOutlined  onClick={()=>{like(0)}} key='dislike'/>,
+          <Collapse
+            key='collapse'
+            bordered={false}
+            style={{backgroundColor:"white"}}
+            showArrow={false}
+            items={[
+             {
+              key: '1',
+              label: <CommentOutlined key='comments'/>,
+              children: <List
+              style={{width:'300%', right: "220%", backgroundColor: 'white'}}
+              itemLayout='horizontal'
+              bordered
+              dataSource={[
+                {
+                  username: 'Test User One',
+                  comment: 'test comment one'
+                },
+                {
+                  username: 'Test User Two',
+                  comment: 'test comment two'
+                }
+              ]}
+              renderItem={(item) => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar= {<Avatar src={Cheesieburger}/>}
+                    title={<p>{item.username}</p>}
+                    description={item.comment}
+                  />
+                </List.Item>
+              )}
+              />
+              }
+            ]}
+          />
+        ]}
+        hoverable
+        >
+        <Meta
+          avatar={<Avatar src={Cheesieburger} />}
+          title={postTitle}
+          description='A Royale with Cheese'
+        />
+        <Button onClick={savePost} type='primary' icon={<HeartOutlined />} style={{position: 'absolute', top:'1%', right: '2%'}} />
+        <Collapse items={[
+          {
+            key: '1',
+            label: 'Nutritional Info',
+            children: <p>Some info that is very nutritious</p>
+          },
+          {
+            key: '2',
+            label: 'Ingredients',
+            children: <p>Definitely not random pieces of food from the trash</p>
+          },
+          {
+            key: '3',
+            label: 'Recipe/Steps',
+            children: <p>Get food, add fire, ?, profit</p>
+          },
+          {
+            key: '4',
+            label: 'Comments',
+            children: <p>This heckin sick bro</p>
+          }
+        ]}
+        bordered={false}
+        style={{backgroundColor:"white", paddingTop:'10px'}}
+        />
+      </Card>
+      {commentsOpen &&
+        <List
+        itemLayout='horizontal'
+        bordered
+        dataSource={[
+          {
+            username: 'Test User One',
+            comment: 'test comment one'
+          },
+          {
+            username: 'Test User Two',
+            comment: 'test comment two'
+          }
+        ]}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar= {<Avatar src={Cheesieburger}/>}
+              title={<p>{item.username}</p>}
+              description={item.comment}
+            />
+          </List.Item>
+        )}
+        />
+      }
+    </div>
   )
 }

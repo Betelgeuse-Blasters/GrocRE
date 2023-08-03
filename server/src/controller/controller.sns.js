4f8832078d902cec8d1f81ee430ce25abdef5760
Kiel Fuller <kpfuller28@gmail.com>
2023-08-03 11:43:46 -0400

    feat: sns routes setup, feed renders all posts with relevant data

--- server/src/controller/controller.sns.js
+++ server/src/controller/controller.sns.js
@@ -4,10 +4,21 @@ import { validationResult } from "express-validator";
 
 export async function getAllPosts(req, res) {
   try {
-    console.log(req.body);
-    res.sendStatus(201)
+    const posts = await model.getAllPosts();
+    res.status(200).send(posts)
   } catch (err) {
-    console.log('get all posts error: ', err)
+    res.status(550).send(err)
+  }
+}
+
+export async function getPost(req, res) {
+  console.log(req.query);
+  try {
+    const user = await model.getUser(req.query.userid);
+    const meal = await model.getRecipe(req.query.mealid);
+    const response = {username: user.username, meal: meal}
+    res.status(202).send(response);
+  } catch (err) {
     res.status(550).send(err)
   }
 }
